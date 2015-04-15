cb331f8faf1dd399c6837e7479ea43035e1f4267
conc <yueguangsenvzixiang@foxmail.com>
2015-04-15 11:40:03 +0800

    Create WordLevenShtein.h

---
+++ WordLevenShtein.h
@@ -0,0 +1,9 @@
+#ifndef _WordLenvenShtein_H_
+#define  _WordLenvenShtein_H_
+ 
+#include "func.h"
+ 
+double GetLevenshteinRadio(string a, string b);
+int CalWordArrayRadio(string Word[],int n);
+ 
+#endif
