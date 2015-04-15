0d3ca0c36f36348ffdb0b74e1c552297b8c5df6c
conc <yueguangsenvzixiang@foxmail.com>
2015-04-15 11:35:38 +0800

    Create func.h

---
+++ func.h
@@ -0,0 +1,16 @@
+#ifndef _FUNCS_H_
+#define _FUNCS_H_
+ 
+#include <string>
+#include <iostream>
+ 
+using namespace std;
+ 
+#define ERROR -1;
+ 
+int GetMaxValue(int a, int b);
+int GetMinValue(int a, int b, int c);
+int IsNotSame(char a, char b);
+ 
+ 
+#endif
